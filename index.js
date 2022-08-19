a5f2235369fdf1bde762ef7122af76030e99dbcb
sharon <sharon.chepkemoi@student.moringaschool.com>
2022-08-19 17:43:43 +0300

    completed assgnmemt

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const block = 42;
+    const blocksNumber = location < 42 ? block -location: location -block
+    return blocksNumber;
+    }
+
+    function distanceFromHqInFeet(blockNumber) {
+        return distanceFromHqInBlocks(blockNumber) * 264; 
+        
+    }
+    function distanceTravelledInFeet(start, destination){
+        const distance = start < destination ? (destination - start)* 264 : (start - destination)* 264;
+        return distance;
+
+    }
+    function calculatesFarePrice(start, destination){
+        const distance = distanceTravelledInFeet(start, destination);
+        if(distance <= 400){
+            return 0;
+        } else if ( distance > 400 && distance <= 2000){
+            return 0.02*(distance - 400);
+        }else if (distance > 2000 && distance < 2500 ) {
+            return 25;
+            
+        }else {
+            return 'cannot travel that far';
+            
+           }
+        }
